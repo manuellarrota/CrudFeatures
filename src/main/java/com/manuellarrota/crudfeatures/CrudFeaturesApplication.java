c72f07fead56e99a8af13e9d5e0bdcbd5898350e
Manuel Larrota <manuel.larrota@gmail.com>
2024-10-16 10:15:16 -0400

    Commit inicial:
    - Se crea proyecto base con springInit

---
+++ src/main/java/com/manuellarrota/crudfeatures/CrudFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.manuellarrota.crudfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuellarrota/crudfeatures/CrudFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuellarrota.crudfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
